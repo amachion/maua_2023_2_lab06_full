452372a4d153780cf412257246e589aec67c1a88
Andreia Cristina Grisolio Machion <andreia.machion@maua.br>
2023-11-13 08:25:45 -0300

    até código 2.21.4

--- backend.js
+++ backend.js
@@ -4,6 +4,7 @@
 const express = require("express");
 const cors = require("cors");
 const mongoose = require('mongoose');
+const uniqueValidator = require('mongoose-unique-validator');
 const app = express();
 app.use(express.json());
 app.use(cors());
@@ -13,22 +14,16 @@ const Filme = mongoose.model ("Filme", mongoose.Schema({
   sinopse: {type: String}
 }))
 
-async function conectarMongoDB () {
-  await mongoose.connect (`mongodb+srv://pro_mac:mongo123@cluster0.skf8n.mongodb.net/?retryWrites=true&w=majority`)
-}
+const usuarioSchema = mongoose.Schema ({
+  login: {type: String, required: true, unique:true},
+  senha: {type: String, required:true}
+})
+usuarioSchema.plugin(uniqueValidator);
+const Usuario = mongoose.model("Usuario", usuarioSchema);
 
-// let filmes = [
-//   {
-//     titulo: "Forrest Gump - O Contador de Histórias",
-//     sinopse:
-//       "Quarenta anos da história dos Estados Unidos, vistos pelos olhos de Forrest Gump (Tom Hanks), um rapaz com QI abaixo da média e boas intenções.",
-//   },
-//   {
-//     titulo: "Um Sonho de Liberdade",
-//     sinopse:
-//       "Em 1946, Andy Dufresne (Tim Robbins), um jovem e bem sucedido banqueiro, tem a sua vida radicalmente modificada ao ser condenado por um crime que nunca cometeu, o homicídio de sua esposa e do amante dela",
-//   },
-// ];
+async function conectarMongoDB () {
+  await mongoose.connect (`mongodb+srv://pro_mac:mongo1234@cluster0.skf8n.mongodb.net/?retryWrites=true&w=majority`)
+}
 
 //ponto de acesso http://localhost:3000/ola
 app.get("/ola", (req, res) => {
@@ -46,12 +41,18 @@ app.post("/filmes", async (req, res) => {
     const sinopse = req.body.sinopse;
     //monta o objeto filme com esses 2 atributos
     const filme = new Filme({titulo: titulo, sinopse: sinopse});
-    //filmes.push(filme)
-    
     await filme.save()
-    
     const filmes = await Filme.find();
     res.json(filmes);
+})
+
+app.post("/signup", async (req, res) => {
+  const login = req.body.login;
+  const senha = req.body.senha;
+  const usuario = new Usuario({login: login, senha: senha});
+  const respMongo = await usuario.save();
+  console.log(respMongo);
+  
 })
 
 app.listen(3000, () => {
